45af4f203be8bafe8bd3f4f1d5db2c06834c3097
Jackwang-lea <canikissyoui.0424@gmail.com>
2025-06-16 20:29:12 +0800

    Initial Commit

---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by zhetaoWang on 2025/6/16.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ newTests/newTests.swift
@@ -0,0 +1,17 @@
+//
+//  newTests.swift
+//  newTests
+//
+//  Created by zhetaoWang on 2025/6/16.
+//
+
+import Testing
+@testable import new
+
+struct newTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
